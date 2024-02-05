0da3e3738d09d07498f5fa987be895db5b19acda
chintakrindasaisrinivas <119840621+chintakrindasaisrinivas@users.noreply.github.com>
2024-02-05 11:26:56 +0530

    import java.util.*; public class Cs{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int c=sc.nextInt();         int d=sc.nextInt();         if(a+b<c+d)         {             System.out.printf("%d",a+b);         }         else         {             System.out.printf("%d",c+d);         }     } }

---
+++ Interior_Design.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Cs{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=sc.nextInt();
+        if(a+b<c+d)
+        {
+            System.out.printf("%d",a+b);
+        }
+        else
+        {
+            System.out.printf("%d",c+d);
+        }
+    }
+}
