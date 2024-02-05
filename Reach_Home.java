c60e212a53fc410416c16382d411d290ddad71db
chintakrindasaisrinivas <119840621+chintakrindasaisrinivas@users.noreply.github.com>
2024-02-05 10:33:08 +0530

    import java.util.*; public class Cs{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         if(a*5>=b)         {             System.out.print("YES");         }         else         {             System.out.print("NO");         }     } }

---
+++ Reach_Home.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Cs{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a*5>=b)
+        {
+            System.out.print("YES");
+        }
+        else
+        {
+            System.out.print("NO");
+        }
+    }
+}
