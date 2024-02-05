74ed28464481997d47d556770741fa1ac4a12537
chintakrindasaisrinivas <119840621+chintakrindasaisrinivas@users.noreply.github.com>
2024-02-05 10:36:06 +0530

    import java.util.*; public class Cs{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         if(a<b*2)         {             System.out.print("NO");         }         else         {             System.out.print("YES");         }     } }

---
+++ Good_Investment__or__Not.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Cs{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a<b*2)
+        {
+            System.out.print("NO");
+        }
+        else
+        {
+            System.out.print("YES");
+        }
+    }
+}
