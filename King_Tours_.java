89c64fb24b9e901393369a1025e70777131cf0a5
chintakrindasaisrinivas <119840621+chintakrindasaisrinivas@users.noreply.github.com>
2024-02-07 21:20:13 +0530

    import java.util.*; public class sai {     public static void main(String[] args)     {         Scanner gopi=new Scanner(System.in);         int a=gopi.nextInt();         int b=gopi.nextInt();         int c=(5*a)+(7*b);         System.out.println(c);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class sai
+{
+    public static void main(String[] args)
+    {
+        Scanner gopi=new Scanner(System.in);
+        int a=gopi.nextInt();
+        int b=gopi.nextInt();
+        int c=(5*a)+(7*b);
+        System.out.println(c);
+    }
+}
