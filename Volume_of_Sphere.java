c1ba03b75742af91003b1e5acbcab6e4769e0ff5
chintakrindasaisrinivas <119840621+chintakrindasaisrinivas@users.noreply.github.com>
2024-02-07 21:04:23 +0530

    import java.util.*; public class sc {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         double b=1.3333333333333*3.14*a*a*a;         System.out.printf("%.2f",b);     } }

---
+++ Volume_of_Sphere.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class sc
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        double b=1.3333333333333*3.14*a*a*a;
+        System.out.printf("%.2f",b);
+    }
+}
