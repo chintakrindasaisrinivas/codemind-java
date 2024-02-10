67f57aade3027db05aadea4fbdcee3c1be5cd16c
chintakrindasaisrinivas <119840621+chintakrindasaisrinivas@users.noreply.github.com>
2024-02-10 14:01:13 +0530

    import java.util.*; public class Cs{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();        // int a=Integer.parseInt(s,8)         System.out.print(Integer.parseInt(s,8));     } }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Cs{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+       // int a=Integer.parseInt(s,8)
+        System.out.print(Integer.parseInt(s,8));
+    }
+}
